2bdc7616ff35cd55ff1442ae6718ff35d53af095
Yangbo Liu <geeksoap@outlook.com>
2024-06-28 22:06:59 -0400

    Login and Signup(2/3) init

--- src/app/pages/choose-plan/choose-plan.component.ts
+++ src/app/pages/choose-plan/choose-plan.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-choose-plan',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './choose-plan.component.html',
   styleUrl: './choose-plan.component.scss'
 })
 export class ChoosePlanComponent {
+
+  constructor(private router: Router) { };
+
+  toHome() {
+    this.router.navigate(['/']);
+  }
 
 }
