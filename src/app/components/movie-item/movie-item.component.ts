c32719fa462682c3e6351e15e5596a213f3fdbca
Yangbo Liu <geeksoap@outlook.com>
2024-06-30 23:44:00 -0400

    blur effect

--- src/app/components/movie-item/movie-item.component.ts
+++ src/app/components/movie-item/movie-item.component.ts
@@ -1,5 +1,5 @@
 import { NgStyle } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-movie-item',
@@ -11,4 +11,14 @@ import { Component, Input } from '@angular/core';
 export class MovieItemComponent {
   @Input() movie: any;
 
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngOnInit() {
+    const movieItemElement = this.el.nativeElement.querySelector('.movie-item');
+    this.renderer.setStyle(
+      movieItemElement,
+      'background-image',
+      `url('https://image.tmdb.org/t/p/w500${this.movie.poster_path}')`
+    );
+  }
 }
