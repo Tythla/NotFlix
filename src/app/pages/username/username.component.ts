7d39d69f4650421cf4f51778668803fb2d14c561
Yangbo Liu <geeksoap@outlook.com>
2024-07-05 11:16:25 -0400

    username Reactive Form

--- src/app/pages/username/username.component.ts
+++ src/app/pages/username/username.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SignupService } from '../../services/signup.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-username',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,ReactiveFormsModule, CommonModule],
   templateUrl: './username.component.html',
   styleUrl: './username.component.scss'
 })
 export class UsernameComponent {
 
-  constructor(private router: Router) { };
+  constructor(private router: Router, public signupService:SignupService) { };
 
-  toMovieList() { };
+  onSubmit() {
+    if (this.signupService.signupForm.controls['username'].valid && this.signupService.signupForm.controls['api'].valid) {
+      console.log(this.signupService.signupForm.value)
+      window.alert('Signup successful!')
+    }
+  }
+
+  toMovieList(){}
 
 }
