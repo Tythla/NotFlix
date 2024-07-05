a09b7f9319875b219909e7066698534ccd6c9959
Yangbo Liu <geeksoap@outlook.com>
2024-07-05 11:06:51 -0400

    choose-plan Reactive Form

--- src/app/pages/choose-plan/choose-plan.component.ts
+++ src/app/pages/choose-plan/choose-plan.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { SignupService } from '../../services/signup.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-choose-plan',
   standalone: true,
-  imports: [RouterModule,NavbarComponent],
+  imports: [RouterModule,NavbarComponent,ReactiveFormsModule],
   templateUrl: './choose-plan.component.html',
   styleUrl: './choose-plan.component.scss'
 })
 export class ChoosePlanComponent {
 
-  constructor(private router: Router) { };
+  constructor(private router: Router, public signupService:SignupService) { };
 
   toUsername() {
     this.router.navigate(['username']);
+  }
+
+  onSubmit() {
+    if (this.signupService.signupForm.controls['plan'].valid) {
+      this.router.navigate(['username']);
+    }
   }
 
 }
