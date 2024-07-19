ac09267d6b06d290baa9322eec1f6fe1c7523457
Yangbo Liu <geeksoap@outlook.com>
2024-07-19 16:20:16 -0400

    interceptors

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AuthInterceptor } from './app/interceptors/auth.service';
 
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(routes),
-    provideHttpClient(), provideAnimationsAsync()
+    provideHttpClient(), provideAnimationsAsync(),
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
   ]
 })
   .catch((err) => console.error(err));
